28444c2edaf4c54ef184678053a6d2b0e72e470e
sweta-2003 <85947160+sweta-2003@users.noreply.github.com>
2023-05-31 16:16:50 +0000

    added dynamic data and routes

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flaskflaskapp.sweta-2003.repl.co
+from flask import Flask,render_template,jsonify
 app=Flask(__name__)
-
+JOBS=[
+  {
+   'id':1,
+    'title':'Data Scientist',
+    'Salary':'Rs.1000000',
+    'location':'Bangalore, India'
+  },
+   {
+   'id':2,
+    'title':'Backend Engineer',
+   
+    'location':'Remote'
+  },
+   {
+   'id':3,
+    'title':'Frontend Engineer',
+    'Salary':'$800000',
+    'location':'Canada'
+  },
+   {
+   'id':4,
+    'title':'Data Analyst',
+    'Salary':'$700000',
+    'location':'Japan'
+  }
+]
 @app.route("/")
 def hellow():
-  return "<p>hellow world</p>"
+  return  render_template("home.html",jobs=JOBS,company_name='Jovian')
 
+@app.route("/api/jobs")
+def json_list():
+  return jsonify(JOBS)
 if __name__=="__main__":
    app.run(host='0.0.0.0',debug=True)
